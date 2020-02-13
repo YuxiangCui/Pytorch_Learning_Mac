918c943fa95c0b2d0049626f8d6ee6cf4258b24d
softfishnotdelicious <529555875@qq.com>
2020-02-13 12:06:59 +0800

    02_MNIST_data_processing

--- pytorch_learning_02_MNIST/pytorch_learning_02_MNIST.py
+++ pytorch_learning_02_MNIST/pytorch_learning_02_MNIST.py
@@ -1,1 +1,41 @@
+# TRAIN
 import torch
+from torch import nn
+from torch.nn import functional as F
+from torch import optim
+
+import torchvision
+from torchvision import transforms as transforms
+from matplotlib import pyplot as plt
+
+from utils import plot_figure, plot_image, one_hot
+
+batch_size = 512
+
+
+# Load dataset
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST(
+        'mnist_data',
+        train=True,
+        download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+    batch_size=batch_size,
+    shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST(
+        'mnist_data/',
+        train=False,
+        download=True,
+        transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+    batch_size=batch_size,
+    shuffle=False)
+
+x, y = next(iter(train_loader))
+print(x.shape, y.shape, x.min(), x.min())
---
+++ pytorch_learning_02_MNIST/utils.py
@@ -0,0 +1,33 @@
+import torch
+from matplotlib import pyplot as plt
+
+
+# training 过程中 loss 下降过程
+def plot_figure(data):
+    fig = plt.figure()
+    plt.plot(range(len(data)), data, color="blue")
+    plt.legend(['value'], loc='upper right')
+    plt.xlabel('step')
+    plt.ylabel('value')
+    plt.show()
+
+
+# 展示识别的结果
+def plot_image(img, label, name):
+    fig = plt.figure()
+    for i in range(6):
+         plt.subplot(2, 3, i + 1)
+         plt.tight_layout()
+         plt.imshow(img[i][0]*0.3081+0.1307, cmap='gray', interpolation='none')
+         plt.title("{}: {}".format(name, label[i].item()))
+         plt.xticks([])
+         plt.yticks([])
+    plt.show()
+
+
+# one hot 编码
+def one_hot(label, depth=10):
+    out = torch.zero_(label.size(0), depth)
+    idx = torch.LongTensor(label).view(-1,1)
+    out.scatter_(dim=1, index=idx, value=1)
+    return out
