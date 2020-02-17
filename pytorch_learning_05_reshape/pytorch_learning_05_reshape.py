8ed39b8868ddb2e11a21c2cf95ad8f8570fd2178
YuxiangCui <21932111@zju.edu.cn>
2020-02-17 22:48:05 +0800

    05_reshape start

---
+++ pytorch_learning_05_reshape/pytorch_learning_05_reshape.py
@@ -0,0 +1,20 @@
+import torch
+
+a = torch.rand(4, 1, 28, 28)
+print(a.shape)
+# view / reshape
+print(a.view(4, 28*28))
+
+# squeeze / un-squeeze
+
+
+# transpose / t / permute
+
+
+# expand / repeat
+
+
+
+
+
+
