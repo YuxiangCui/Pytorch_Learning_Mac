a02ca76289ea7cb24c5e1e9771c2488fb142b0f5
YuxiangCui <21932111@zju.edu.cn>
2020-02-18 21:07:35 +0800

    pytorch_learning_07_merge_split done

--- pytorch_learning_07_merge_split/pytorch_learning_07_merge_split.py
+++ pytorch_learning_07_merge_split/pytorch_learning_07_merge_split.py
@@ -25,4 +25,34 @@ print(d.shape)  # torch.Size([2, 4, 3, 16, 16])
 # print(torch.stack([a, b_0], dim=0))  BUG:stack对应的元素必须相同
 
 
+# num*len
+# split
+# 按照长度进行分割
+# 前面标注分割的结果，以及对应的变量名，后面写清分割的方案或者片段长度
+a = torch.rand(32, 8)
+b = torch.rand_like(a)
+e = torch.rand_like(a)
 
+c = torch.stack([a, b], dim=0)
+d = torch.stack([a, b, e], dim=0)
+print(c.shape)
+print(d.shape)
+
+aa, bb = c.split([1, 1], dim=0)
+print(aa.shape)
+print(bb.shape)
+
+aa, bb = d.split([2, 1], dim=0)
+print(aa.shape)
+print(bb.shape)
+
+aa, bb = c.split(1, dim=0)
+print(aa.shape)
+print(bb.shape)
+
+# chunk
+# 按照分段数目进行分割
+
+aa, bb = c.chunk(2, dim=0)
+print(aa.shape)
+print(bb.shape)
