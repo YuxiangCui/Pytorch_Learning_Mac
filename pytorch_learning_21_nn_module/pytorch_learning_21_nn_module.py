1bd7a1ad26f5ceee62b4cd703146d303c2c96722
YuxiangCui <21932111@zju.edu.cn>
2020-02-26 23:58:11 +0800

    pytorch_learning_21_nn_module working

--- pytorch_learning_21_nn_module/pytorch_learning_21_nn_module.py
+++ pytorch_learning_21_nn_module/pytorch_learning_21_nn_module.py
@@ -1,3 +1,13 @@
 # pytorch_learning_21_nn_module
 
+# nn.module
+# Linear, ReLU, Sigmoid...模块
+# nn.Sequential 网络容器
+# 更好的网络参数管理 list(net.parameters())  list(net.named_parameters())  或者替换list为dict
+# 调用参数也方便 optimizer = optim.SGD(net.parameters(), lr=1e-3)
+# children 直接连接的子节点，modules 所有的节点
+# to(device)命令  注意区别模块和tensor的使用
+# save & load
+# train & test
+# 自定义的话也要继承自 nn.module
 import torch
