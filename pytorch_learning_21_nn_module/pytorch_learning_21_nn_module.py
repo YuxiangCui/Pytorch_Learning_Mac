98c5f4cd939b2d49bb8764c9ca71e64bc6bf6324
YuxiangCui <21932111@zju.edu.cn>
2020-02-27 00:03:30 +0800

    pytorch_learning_21_nn_module done

--- pytorch_learning_21_nn_module/pytorch_learning_21_nn_module.py
+++ pytorch_learning_21_nn_module/pytorch_learning_21_nn_module.py
@@ -9,5 +9,6 @@
 # to(device)命令  注意区别模块和tensor的使用
 # save & load
 # train & test
-# 自定义的话也要继承自 nn.module
+# 自定义的话也要继承自 nn.module 例如nn里面没有的flatten操作层，reshape操作等
+# nn.Parameter()包装自己的变量进入优化器参数内，require_grad
 import torch
