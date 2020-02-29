9a74599feb761d38bad33a48b164b87c1e5716bf
YuxiangCui <21932111@zju.edu.cn>
2020-02-29 16:26:21 +0800

    pytorch_learning_23_ResNet done and modified

--- pytorch_learning_23_ResNet/ResNet_train.py
+++ pytorch_learning_23_ResNet/ResNet_train.py
@@ -1,33 +1,34 @@
+# github 参考自别人代码，增加一些网络优化
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 import argparse
-from resnet import ResNet18
+from pytorch_learning_23_ResNet import ResNet
 
 # 定义是否使用GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# 参数设置,使得我们能够手动输入命令行参数，就是让风格变得和Linux命令行差不多
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--outf', default='./model/', help='folder to output images and model checkpoints') #输出结果保存路径
-parser.add_argument('--net', default='./model/Resnet18.pth', help="path to net (to continue training)")  #恢复训练时的模型路径
-args = parser.parse_args()
+
+# parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+# parser.add_argument('--outf', default='./model/', help='folder to output images and model checkpoints')
+# parser.add_argument('--net', default='./model/Resnet18.pth', help="path to net (to continue training)")  #恢复训练时的模型路径
+# args = parser.parse_args()
 
 # 超参数设置
-EPOCH = 1   # 遍历数据集次数
-pre_epoch = 0  # 定义已经遍历数据集的次数
-BATCH_SIZE = 128      # 批处理尺寸(batch_size)
-LR = 0.01        # 学习率
+EPOCH = 1
+pre_epoch = 0
+BATCH_SIZE = 128
+LR = 0.01
 
 
-# 准备数据集并预处理
 transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
-    transforms.RandomHorizontalFlip(),  #图像一半的概率翻转，一半的概率不翻转
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)), #R,G,B每层的归一化用到的均值和方差
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 transform_test = transforms.Compose([
@@ -44,16 +45,16 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False,
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 # 模型定义-ResNet
-net = ResNet18().to(device)
+net = ResNet().to(device)
 
-# 定义损失函数和优化方式
-criterion = nn.CrossEntropyLoss()  #损失函数为交叉熵，多用于多分类问题
-optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4) #优化方式为mini-batch momentum-SGD，并采用L2正则化（权重衰减）
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4)
 
 # 训练
 if __name__ == "__main__":
-    best_acc = 85  #2 初始化best test accuracy
-    print("Start Training, Resnet-18!")  # 定义遍历数据集的次数
+    best_acc = 85
+    print("Start Training, Resnet!")  # 定义遍历数据集的次数
     with open("acc.txt", "w") as f:
         with open("log.txt", "w")as f2:
             for epoch in range(pre_epoch, EPOCH):
@@ -87,7 +88,7 @@ if __name__ == "__main__":
                     f2.write('\n')
                     f2.flush()
 
-                # 每训练完一个epoch测试一下准确率
+
                 print("Waiting Test!")
                 with torch.no_grad():
                     correct = 0
@@ -103,13 +104,13 @@ if __name__ == "__main__":
                         correct += (predicted == labels).sum()
                     print('测试分类准确率为：%.3f%%' % (100 * correct / total))
                     acc = 100. * correct / total
-                    # 将每次测试结果实时写入acc.txt文件中
+
                     print('Saving model......')
                     torch.save(net.state_dict(), '%s/net_%03d.pth' % (args.outf, epoch + 1))
                     f.write("EPOCH=%03d,Accuracy= %.3f%%" % (epoch + 1, acc))
                     f.write('\n')
                     f.flush()
-                    # 记录最佳测试分类准确率并写入best_acc.txt文件中
+
                     if acc > best_acc:
                         f3 = open("best_acc.txt", "w")
                         f3.write("EPOCH=%d,best_acc= %.3f%%" % (epoch + 1, acc))
