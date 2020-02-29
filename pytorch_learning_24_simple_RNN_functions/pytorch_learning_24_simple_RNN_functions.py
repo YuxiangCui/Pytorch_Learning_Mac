6d6593260cef7da317fc9d8b05f197280c450ca2
YuxiangCui <21932111@zju.edu.cn>
2020-02-29 23:05:44 +0800

    pytorch_learning_24_simple_RNN_functions working

--- pytorch_learning_24_simple_RNN_functions/pytorch_learning_24_simple_RNN_functions.py
+++ pytorch_learning_24_simple_RNN_functions/pytorch_learning_24_simple_RNN_functions.py
@@ -5,17 +5,54 @@ import torch.optim as optim
 from matplotlib import pyplot as plt
 
 '''
-    rnn = nn.RNN(100,10)
+    rnn = nn.RNN(100,10)  
+    # input size(embedding 的维度，表达向量的维度), hidden size(隐藏层维度), num layers RNN层数，默认是1
+    
+    out, ht = forward(x, h0)
+    # x: [seq_len, b, word_vec] 一次性给定所有输入 e.g. [5, 3, 100] 三句话，每句话五个单词长度，每个单词用一个100维向量表示
+    # h0 / ht: [num layers, b, h dim] h0可以不提供，默认全零开始， ht为最后的返回隐藏层状态
+    # out: [seq_len, b, h dim] 所有的h
+    
     rnn._parameters.keys()
     print(rnn._parameters.keys())
 
-
-    rnn = nn.RNN(input_size=100,hidden_size=20,num_layers=1)
+    # 单层RNN
+    rnn = nn.RNN(input_size=100, hidden_size=20, num_layers=1)
     print(rnn)
-    x = torch.randn(10,3,100)  #维度对应：单词个数，batch，单词维度
-    out,h = rnn(x,torch.zeros(1,3,20))
+    x = torch.randn(10,3,100)  # 单词个数，batch_size，单词向量维度
+    out, h = rnn(x,torch.zeros(1,3,20))
     print(out.shape,h.shape)
-    #torch.Size([10, 3, 20]) torch.Size([1, 3, 20])
+    # torch.Size([10, 3, 20]) torch.Size([1, 3, 20])
+    
+    --------------------------------------------------------------------------------
+    # RNN层数加深的话
+    # h[0]增大，对应层数增大，但是out不变，只返回最后一层RNN的h
+    # out为时间序列方向，横向，所有，ht为最后时刻所有的RNN隐藏层参数，纵向，所有
+    
+    rnn = nn.RNN(input_size=100, hidden_size=20, num_layers=4)
+    print(rnn)
+    x = torch.randn(10,3,100)  # 单词个数，batch_size，单词向量维度
+    out, h = rnn(x,torch.zeros(4,3,20))
+    print(out.shape,h.shape)
+    # torch.Size([10, 3, 20]) torch.Size([4, 3, 20])
+    
+    --------------------------------------------------------------------------------
+
+    nn.RNNCell
+    # 逐个时间戳数据输入
+    # input size(embedding 的维度，表达向量的维度), hidden size(隐藏层维度), num layers RNN层数，默认是1
+    ht = rnncell(xt, ht_1)
+    xt: [batch_size, word_vec]
+    ht_1 / ht: [num_layers, batch_size, h_dim]
+    out: torch.stack([h1, h2, ..., ht])
+    
+    cell1 = nn.RNNCell(100, 20)
+    h1 = torch.zeros(3, 20)
+    for xt in x:
+        h1 = cell1(xt, h1)
+    print(h1.shape)
+    # torch.Size([3,20])
+    
 '''
 num_time_steps = 50
 input_size = 1
