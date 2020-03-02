91461e3359d9f6323311c25da1c2be3856e20304
YuxiangCui <21932111@zju.edu.cn>
2020-03-02 23:53:12 +0800

    pytorch_learning_27_DIY_Dataset_CNN start

--- pytorch_learning_26_simple_Sentiment_Classification/pytorch_learning_26_simple_Sentiment_Classification.py
+++ pytorch_learning_26_simple_Sentiment_Classification/pytorch_learning_26_simple_Sentiment_Classification.py
@@ -1,4 +1,18 @@
 # pytorch_learning_26_simple_Sentiment_Classification
 
+# Google CoLab
+# K80 可以使用12h 会清洗
+
 import torch
 import torch.nn as nn
+
+TEXT = data.Field(tokensize='spacy')
+LABEL = data.LabelField(dtype=torch.float)
+
+
+class RNN(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim):
+        super(RNN, self).__init__()
+        self.embbeding = nn.Embedding(vocab_size, embedding_dim)
+        self.rnn = nn.LSTM(embedding_dim, hidden_dim, num_layers=2, bidirectional=True, dropout=0.5)
+        
